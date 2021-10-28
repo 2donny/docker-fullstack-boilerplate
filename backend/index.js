bf516a24c9c2bdf04aa7b2ff4903b37bdeca4cc5
정이든 <2donny@naver.com>
2021-10-28 14:47:20 +0900

    first

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const redis = require("redis");
+const db = require("./db");
+
+// Create Redis client
+// const redisClient = redis.createClient({
+//   host: "redis-server", // docker-compose.yml에 명시한 컨 테이너 이름으로 준다.
+//   port: 6379,
+// });
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+app.get("/api/values", (req, res) => {
+  db.pool.query("SELECT * FROM lists;", (err, result, fields) => {
+    if (err) return res.status(500).send(err);
+    return res.json(result);
+  });
+});
+
+app.post("/api/value", (req, res, next) => {
+  res.json("hello world");
+});
+
+// redisClient.set("number", 0);
+
+// app.get("/", (req, res) => {
+//   redisClient.get("number", (err, number) => {
+//     redisClient.set("number", +number + 1);
+//     res.send(`current count : ${number}`);
+//   });
+// });
+
+app.listen(8080, () => {
+  console.log("✅ Server is running on port 8080");
+});
