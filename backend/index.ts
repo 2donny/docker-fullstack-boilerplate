0d20a0a93252f73f77f6354fffa0fddd1ce6afe2
정이든 <2donny@naver.com>
2021-10-30 16:48:36 +0900

    elastic bean stalk linux v2로  변경 후 적용

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,22 @@
+import { config } from "dotenv";
+config();
 import express from "express";
-import { createConnection } from "typeorm";
+import { createConnection, ConnectionOptions } from "typeorm";
 
-createConnection()
+console.log(process.env.POSTGRES_HOST);
+
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
+  entities: ["*.entity.js,ts"],
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+createConnection(connectionOptions)
   .then(async (connection) => {
-    console.log(connection);
     const app = express();
 
     app.get("/", (req, res) => {
@@ -19,4 +32,4 @@ createConnection()
       console.log("✅ Server is running on port 8080");
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("error : ", err));
